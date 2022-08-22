8997d68c70276dbef5a08feec3f86b8ddad52c32
Mohit Sharma <iamxguy352@gmail.com>
2022-08-22 17:58:48 +0530

    Fixed key issue

--- src/DummyData.jsx
+++ src/DummyData.jsx
@@ -27,25 +27,25 @@ export const sectionOneItems = [
 
 export const sectionTwoItems = [
   {
-    id: 1,
+    id: 5,
     name: "Red sneaker",
     price: "10,000",
     img: "https://png.pngitem.com/pimgs/s/488-4886152_vlone-nike-air-force-1-low-vlone-sneaker.png",
   },
   {
-    id: 2,
+    id: 6,
     name: "Black sneaker",
     price: "8,000",
     img: "https://png.pngitem.com/pimgs/s/488-4886028_shoes-blak-balenciaga-shoes-53045-hd-png-download.png",
   },
   {
-    id: 3,
+    id: 7,
     name: "Green sneaker",
     price: "12,000",
     img: "https://png.pngitem.com/pimgs/s/743-7434835_sneakers-png-download-transparent-png.png",
   },
   {
-    id: 4,
+    id: 8,
     name: "Shiny sneaker",
     price: "11,000",
     img: "https://png.pngitem.com/pimgs/s/190-1902743_sneakers-png-file-blue-men-sneakers-transparent-png.png",
@@ -54,25 +54,25 @@ export const sectionTwoItems = [
 
 export const sectionThreeItems = [
   {
-    id: 1,
+    id: 9,
     name: "White Canvas",
     price: "4,000",
     img: "https://png.pngtree.com/png-clipart/20201209/original/pngtree-casual-shoes-png-image_5640199.jpg",
   },
   {
-    id: 2,
+    id: 10,
     name: "Black Canvas",
     price: "6,000",
     img: "https://png.pngtree.com/png-clipart/20201209/original/pngtree-casual-shoes-png-image_5631256.jpg",
   },
   {
-    id: 3,
+    id: 11,
     name: "Red Canvas",
     price: "5,000",
     img: "https://png.pngtree.com/png-clipart/20210516/original/pngtree-canvas-shoes-exercise-trend-leisure-png-image_6282301.jpg",
   },
   {
-    id: 4,
+    id: 12,
     name: "Long Canvas",
     price: "8,000",
     img: "https://png.pngtree.com/png-clipart/20210516/original/pngtree-canvas-shoes-exercise-trend-png-image_6282190.jpg",
@@ -81,25 +81,25 @@ export const sectionThreeItems = [
 
 export const sectionFourItems = [
   {
-    id: 1,
+    id: 13,
     name: "Green Led",
     price: "5,000",
     img: "https://www.seekpng.com/png/full/809-8091216_led-shoe-png-sneakers.png",
   },
   {
-    id: 2,
+    id: 14,
     name: "Purple Led",
     price: "7,000",
     img: "https://www.seekpng.com/png/full/291-2916800_galaxy-led-shoes-light-up-usb-charging-high.png",
   },
   {
-    id: 3,
+    id: 15,
     name: "Blue Led",
     price: "9,000",
     img: "https://www.seekpng.com/png/full/361-3611015_galaxy-led-shoes-high-top-light-up-sneakers.png",
   },
   {
-    id: 4,
+    id: 16,
     name: "Pink",
     price: "11,000",
     img: "https://www.seekpng.com/png/full/413-4135423_light-up-shoes-png.png",
